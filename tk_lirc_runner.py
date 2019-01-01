b2f3178bbfeb42fa16efd6d7f4015a06e772b0da
shanmugara <psraj@optonline.net>
2019-01-01 00:06:20 -0500

    Initial push

--- tk_lirc_runner.py
+++ tk_lirc_runner.py
@@ -45,49 +45,49 @@ class HomeScreen(object):
         if dev_name == 'All':
             self.clearcenter()
             self.on_button = Button(self.centerframe, text='ON', command=self.pwr_on_all, fg="green",
-                                    activebackground='yellow', height=2, width=10)
+                                    activebackground='yellow', height=2, width=10, bg='#81BEF7')
             self.on_button.grid(row=5, column=3, rowspan=2)
             self.off_button = Button(self.centerframe, text='OFF', command=self.pwr_off_all, fg="red",
-                                     activebackground='yellow', height=2, width=10)
+                                     activebackground='yellow', height=2, width=10, bg='#81BEF7')
             self.off_button.grid(row=5, column=10, rowspan=2)
 
         elif dev_name == 'Bluray':
             self.clearcenter()
             self.on_button = Button(self.centerframe, text='ON', command=self.rc_bluray.pwr_on, fg="green",
-                                    activebackground='yellow', height=2, width=10)
+                                    activebackground='yellow', height=2, width=10, bg='#81BEF7')
             self.on_button.grid(row=2, column=1, rowspan=2)
             self.off_button = Button(self.centerframe, text='OFF', command=self.rc_bluray.pwr_off, fg="red",
-                                     activebackground='yellow', height=2, width=10)
+                                     activebackground='yellow', height=2, width=10, bg='#81BEF7')
             self.off_button.grid(row=2, column=2, rowspan=2)
             self.on_button = Button(self.centerframe, text='Play', command=self.rc_bluray.pwr_on, fg="blue",
-                                    activebackground='yellow', height=2, width=10)
+                                    activebackground='yellow', height=2, width=10, bg='#81BEF7')
             self.on_button.grid(row=8, column=1, rowspan=1)
             self.on_button = Button(self.centerframe, text='Stop', command=self.rc_bluray.pwr_on, fg="red",
-                                    activebackground='yellow', height=2, width=10)
+                                    activebackground='yellow', height=2, width=10, bg='#81BEF7')
             self.on_button.grid(row=8, column=2, rowspan=1)
             self.on_button = Button(self.centerframe, text='Pause', command=self.rc_bluray.pwr_on, fg="red",
-                                    activebackground='yellow', height=2, width=10)
+                                    activebackground='yellow', height=2, width=10, bg='#81BEF7')
             self.on_button.grid(row=8, column=3, rowspan=1)
             self.on_button = Button(self.centerframe, text='Eject', command=self.rc_bluray.pwr_on, fg="red",
-                                    activebackground='yellow', height=2, width=10)
+                                    activebackground='yellow', height=2, width=10, bg='#81BEF7')
             self.on_button.grid(row=10, column=2, rowspan=1)
 
         elif dev_name == 'AVR':
             self.clearcenter()
             self.on_button = Button(self.centerframe, text='ON', command=self.rc_avr.pwr_on, fg="green",
-                                    activebackground='yellow', height=2, width=10)
+                                    activebackground='yellow', height=2, width=10, bg='#81BEF7')
             self.on_button.grid(row=5, column=3, rowspan=2)
             self.off_button = Button(self.centerframe, text='OFF', command=self.rc_avr.pwr_off, fg="red",
-                                     activebackground='yellow', height=2, width=10)
+                                     activebackground='yellow', height=2, width=10, bg='#81BEF7')
             self.off_button.grid(row=5, column=10, rowspan=2)
 
         elif dev_name == 'Projector':
             self.clearcenter()
             self.on_button = Button(self.centerframe, text='ON', command=self.rc_projector.pwr_on, fg="green",
-                                    activebackground='yellow', height=2, width=10)
+                                    activebackground='yellow', height=2, width=10, bg='#81BEF7')
             self.on_button.grid(row=5, column=3, rowspan=2)
             self.off_button = Button(self.centerframe, text='OFF', command=self.rc_projector.pwr_off, fg="red",
-                                     activebackground='yellow', height=2, width=10)
+                                     activebackground='yellow', height=2, width=10, bg='#81BEF7')
             self.off_button.grid(row=5, column=10, rowspan=2)
 
         elif dev_name == 'Screen':
