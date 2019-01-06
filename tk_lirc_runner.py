767dcb0136f7f95cd75ca34fc0a525f456df95f0
shanmugara <psraj@optonline.net>
2019-01-05 23:58:35 -0500

    add more images

--- tk_lirc_runner.py
+++ tk_lirc_runner.py
@@ -88,7 +88,7 @@ class HomeScreen(object):
             self.eject_button = Button(self.centerframe, text='Eject', command=self.rc_bluray.pwr_on, fg="red",
                                        activebackground='#58ACFA', height=40, width=40, bg='#81BEF7',
                                        image=self.image_eject)
-            self.eject_button.grid(row=10, column=2, rowspan=1)
+            self.eject_button.grid(row=8, column=4, rowspan=1)
 
         elif dev_name == 'AVR':
             self.clearcenter()
