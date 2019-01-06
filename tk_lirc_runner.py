9074eb9749c01121402e23de34c4dd433181b142
shanmugara <psraj@optonline.net>
2019-01-06 00:16:43 -0500

    add more images

--- tk_lirc_runner.py
+++ tk_lirc_runner.py
@@ -59,11 +59,11 @@ class HomeScreen(object):
             self.clearcenter()
             self.on_button = Button(self.centerframe, text='ON', command=self.pwr_on_all, fg="green",
                                     activebackground='#58ACFA', height=40, width=40, bg='#81BEF7', image=self.image_on)
-            self.on_button.grid(row=1, column=1, rowspan=2)
+            self.on_button.grid(row=1, column=1, rowspan=2, padx=(20, 20))
             self.off_button = Button(self.centerframe, text='OFF', command=self.pwr_off_all, fg="red",
                                      activebackground='#58ACFA', height=40, width=40, bg='#81BEF7',
                                      image=self.image_off)
-            self.off_button.grid(row=1, column=30, rowspan=2)
+            self.off_button.grid(row=1, column=30, rowspan=2, padx=(20, 20))
 
         elif dev_name == 'Bluray':
             self.clearcenter()
